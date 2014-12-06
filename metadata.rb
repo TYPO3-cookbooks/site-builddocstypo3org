f8036e2d80aeeac0cff391e0af297aefd5aaccc5
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-12-06 17:35:48 +0100

    [BUGFIX] Update CB dependencies to match the production infrastructure

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,14 @@ maintainer_email "fabien.udriot@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures docs.typo3.org"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.3.0"
+version          "1.3.1"
 
 depends "apt", '~> 2.3.0'
 depends "composer", '~> 1.0.5'
 depends "database", '= 1.3.12'
 depends "mysql", '= 1.3.0'
-depends "php"
+depends "php", '~> 1.1.0'
 depends "git", '= 0.9.0'
-depends "apache2", '~> 1.11.0'
+depends "apache2", '~> 2.0.0'
 depends "python", '~> 1.4.6'
 depends "mercurial", '~> 2.0.4'
