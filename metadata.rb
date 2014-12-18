6c13efe74265202eeb1988109f424b02f9d37e0f
Jacob Floyd <cognifloyd@gmail.com>
2014-12-18 04:20:11 -0500

    [TASK] Increase cookbook version number to 1.4

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "fabien.udriot@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures docs.typo3.org"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.3.2"
+version          "1.4.0"
 
 #_packages
 depends "apt", '~> 2.3.0'
